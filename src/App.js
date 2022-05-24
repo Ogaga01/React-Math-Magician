e955cef1ddc97469adb28935adb6126dcd0c273f
Ogaga <iyaraogaga69@gmail.com>
2022-05-24 16:55:43 +0100

    Modify Add function to class Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <Calculator />
-  );
+class App extends React.Component() {
+  constructor(props) {
+    super(props);
+    this.sttate = {};
+  }
+
+  render() {
+    return (
+      <Calculator />
+    );
+  }
 }
 
 export default App;
