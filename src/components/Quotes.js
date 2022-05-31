fb60d55445c3d21a27b6d75598b9ce882b19ca2b
Ogaga <iyaraogaga69@gmail.com>
2022-05-31 15:04:32 +0100

    Add styles to quotes

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -3,7 +3,10 @@ import React from 'react';
 function Quotes() {
   return (
     <>
-      <p>Math</p>
+      <p className="quotes-para">
+        Mathematics is not about numbers, equations, computations or algorithms: it&apos;s about
+        understanding -William Paul Thurston
+      </p>
     </>
   );
 }
